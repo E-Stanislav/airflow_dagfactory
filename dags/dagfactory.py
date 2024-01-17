60bcc44d1015473cc7db1124cad468197f3d614f
E-Stanislav <stas55900@yandex.ru>
2024-01-17 20:27:26 +0100

    [fix] fixxing set depens between tasks + add docs

--- dags/dagfactory.py
+++ dags/dagfactory.py
@@ -1,9 +1,10 @@
 import json
 import os
-from datetime import datetime
+import traceback
 
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.helpers import chain
 
 base_dir = "/opt/airflow"
 
@@ -14,19 +15,35 @@ list_dags = os.listdir(dag_folder)
 
 
 def read_meta(path: str) -> dict:
+    """Base func for reading file and convert to dict object
+
+    Args:
+        path (str): path to meta file
+
+    Returns:
+        dict: meta file
+    """
     with open(path, "r") as f:
         return json.loads(f.read())
 
 
-def make_task(dag, tasks: list):
-    # Dictionary for storing operators
+def make_task(dag: DAG, tasks: list) -> dict:
+    """Making a dags tasks using the path to the meta file
+
+    Args:
+        dag (DAG): airflow dag object
+        tasks (list): list tasks name
+
+    Returns:
+        dict: An object with the name of the operator task and dependent tasks
+    """
     operators = {}
 
     for task in tasks:
         task_meta = read_meta(f"{task_folder}/{task}.json")
 
         operator_type = task_meta["operator_type"]
-        depends_on = task_meta.get("depends_on")
+        depends_on = task_meta.get("depends_on", [])
 
         if operator_type == "DummyOperator":
             operators[task_meta["task_name"]] = {
@@ -40,18 +57,35 @@ def make_task(dag, tasks: list):
 
 
 def set_depends(operators: dict) -> None:
-    
-    for operator_name, operator_content in operators.items():
-        if not operator_content["depends_on"] is None:
-            for depend_task in operator_content["depends_on"]:
-                operators[depend_task]["dag_id"] >> operator_content["dag_id"]
+    """Setting dependencies between tasks in dags
 
-            
-            
+    Args:
+        operators (dict): An object with task name, operator and dependent tasks
+    """
+    try:
+        for operator_name, operator_content in operators.items():
+            if len(operator_content["depends_on"]) > 0:
+                for depend_task in operator_content["depends_on"]:
+                    chain(
+                        operators[depend_task]["dag_id"],
+                        operator_content["dag_id"],
+                    )
+    except KeyError:
+        pass
+    except Exception as e:
+        traceback.print_exc()
 
 
+def make_dag(dag_path: str) -> DAG:
+    """Making a dag using the path to the meta file
 
-def make_dag(dag_path: str):
+    Args:
+        dag_path (str): Path to the dag meta file
+
+    Returns:
+        DAG: airflow dag
+    """
+
     meta_dag = read_meta(f"{dag_folder}/{dag_path}")
 
     dag = DAG(
@@ -64,7 +98,6 @@ def make_dag(dag_path: str):
     tasks = meta_dag["tasks"]
     operators = make_task(dag=dag, tasks=tasks)
     set_depends(operators=operators)
-
     return dag
 
 
